ab42652171569b6218c57637a4206dc2d9597d09
yellyoshua <me@yoshualopez.com>
2022-05-12 12:14:59 -0500

    create script to generate fake cats and dogs content

---
+++ server/scripts/faker-pets.js
@@ -0,0 +1,42 @@
+import faker from "@faker-js/faker";
+import fs from "fs";
+
+const generateDogs = (n_dogs) => {
+    const dogs = [];
+    for (let i = 0; i < n_dogs; i++) {
+        const newDog = {
+            name: faker.animal.dog(),
+            age: faker.random.numeric(2),
+            breed: faker.helpers.arrayElement(["Pooy", "Poodle", "Pug", "Puggle", "Puggle", "Puggle"]),
+            color: faker.helpers.arrayElement(["black", "white", "gray", "brown", "orange"]),
+            weight: faker.random.numeric(2),
+            image: faker.image.animals(),
+            description: faker.lorem.paragraph(1)
+        };
+        dogs.push(newDog);
+    }
+    return dogs;
+}
+const generateCats = (n_cats) => {
+    const cats = [];
+    for (let i = 0; i < n_cats; i++) {
+        const newCat = {
+            name: faker.animal.cat(),
+            age: faker.random.numeric(2),
+            breed: faker.helpers.arrayElement(["Persian", "Siamese", "Maine", "Bengal", "Ragdoll"]),
+            color: faker.helpers.arrayElement(["black", "white", "gray", "brown", "orange"]),
+            weight: faker.random.numeric(2),
+            image: faker.image.cats(),
+            description: faker.lorem.paragraph(1),
+        };
+        cats.push(newCat);
+    }
+    return cats;
+}
+(() => {
+    const cats = generateCats(100);
+    const dogs = generateDogs(100);
+
+    fs.writeFileSync("public/cats-content.json", JSON.stringify({ data: cats}, null, 2));
+    fs.writeFileSync("public/dogs-content.json", JSON.stringify({ data: dogs}, null, 2));
+})()
