a840b09f57f9914e7341464e82549643abca87d3
yellyoshua <me@yoshualopez.com>
2022-05-18 17:50:20 -0500

    remove comments

--- app/src/screens/Modal.jsx
+++ app/src/screens/Modal.jsx
@@ -1,17 +1,13 @@
 import RenderIf from "../components/RenderIf";
 
-export default function Modal({ title, show, children, onClose }) {
+export default function Modal({ show, children, onClose }) {
     return (
         <RenderIf condition={show}>
             <div
                 className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
             >
                 <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                    {/* Close button */}
-                    {/* Modal content */}
-                    {/*content*/}
                     <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                        {/*body*/}
                         <div className="relative p-6 flex-auto">
                         <button
                             onClick={onClose}
